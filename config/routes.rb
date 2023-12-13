fdf07e728855eea6717db86a534d9554e76db078
“MrPragmatic” <michaelgreed2020@gmail.com>
2023-12-13 19:24:23 +0200

    fix: routes fixes

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,6 @@ Rails.application.routes.draw do
   root 'recipes#index'
   get 'users_profile/:id', to: 'users_profiles#show', as: 'user_profile'
   resource :users_profile, only: %i[show edit update destroy]
-  # Define the sign-in route without authentication
-  devise_scope :user do
-    get 'users/sign_in', to: 'devise/sessions#new', as: :new_user_session
-    post 'users/sign_in', to: 'devise/sessions#create', as: :user_session
-  end
   resources :recipes do
     resources :ingredients
   end
